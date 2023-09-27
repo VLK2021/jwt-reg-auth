99abc5da777260a0cdef4e7949583d74ef47ad46
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-09-27 22:50:56 +0300

    created login logout refresh

--- backend/service/token-service.js
+++ backend/service/token-service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+
 const tokenModel = require('../models/token-model');
 
 
@@ -13,6 +14,27 @@ class TokenService {
         }
     }
 
+
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+
     async saveToken(userId, refreshToken) {
         const tokenData = await tokenModel.findOne({user: userId})
         if (tokenData) {
@@ -22,6 +44,18 @@ class TokenService {
 
         const token = await tokenModel.create({user: userId, refreshToken})
         return token;
+    }
+
+
+    async removeToken(refreshToken) {
+        const tokenData = await tokenModel.deleteOne({refreshToken});
+        return tokenData;
+    }
+
+
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({refreshToken});
+        return tokenData;
     }
 
 }
